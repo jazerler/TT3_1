b19d4c19b8cfc7ba1adf5af22f8bbbc754ac0dd4
yangtingsong <e0420552@u.nus.edu>
2023-05-06 11:09:59 +0800

    login page

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -3,8 +3,27 @@ import { useNavigate } from "react-router-dom";
 
 function Login(props) {
     return (
-        <a>hello</a>
+        <div className="App Background">
+            <header>
+              <h1>Expense claims</h1>
+            </header>
+            <form>
+              <div className="input-group">
+                <label htmlFor="username">Username: </label>
+                <input type="text" id="username" />
+              </div>
+              <div className="input-group">
+                <label htmlFor="password">Password: </label>
+                <input type="password" id="password" />
+              </div>
+              <button type="login" className="submit-btn">
+                Log in
+              </button>
+            </form>
+          </div>
     )
 }
+
+
 
 export { Login };

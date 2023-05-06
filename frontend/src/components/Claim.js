d1c6921e722fb4a31d2e793ef5907f051cfca516
ejon <ngejon@hotmail.com>
2023-05-06 14:40:27 +0800

    Added Modify Button

--- frontend/src/components/Claim.js
+++ frontend/src/components/Claim.js
@@ -8,6 +8,13 @@ const Claim = ({claim}) => {
             <p style={{textAlign:'left'}}> {`Status: ${claim.status} |
             Claim ID: ${claim.claimsID} | Currency: ${claim.currency}`}
             </p>
+            <div button
+                    type="button"
+                    className="btn btn-secondary btn-lg modify-modal"
+                    style={{ height: 50, width: 200 }}
+                >
+                    Modify
+                </div>
     
         </div>
     )
